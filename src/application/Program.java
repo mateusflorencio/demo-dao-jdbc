9a6b4187ff38a7c252851015290e810945bfa842
Mateus Florencio <mateusflorencio@outlook.com.br>
2022-01-27 10:28:15 -0300

    Reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,7 +11,8 @@ public class Program {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		Seller seller = sellerDao.findById(1);
+		System.out.println("=== TESTE 1: seller findById ===");
+		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 	}
 	
--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -7,8 +7,8 @@ import model.entities.Seller;
 public interface SellerDao {
 	
 	
-	void insert (Seller obj);
-	void update (Seller obj);
+	void insert (Seller seller);
+	void update (Seller seller);
 	void delete (Seller id);
 	Seller findById(Integer id);
 	List <Seller> findAll();;
